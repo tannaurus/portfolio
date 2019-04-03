079852bb68eb8fd2a8fe9c04e60d4ab59fb9cfbd
Tanner Gill <contacttannergill@gmail.com>
2019-04-03 15:39:04 -0700

    Implemented static contact form

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -2,31 +2,58 @@ import React from "react";
 
 import "./ContactStyles.css";
 
-const Contact = () => (
-  <article id="contact" className="contact">
-    <div className="form-wrapper">
-      <h2>REACH OUT</h2>
-      <form>
-        <div className="client-info">
-          <div className="client-info-col">
-            <label htmlFor="frmNameA">Name</label>
-            <input type="text" name="name" id="frmNameA" autoComplete="name" required placeholder="Bazos" list="names"/>
-            <datalist>
-              <option value="Darth Vader"/>
-              <option value="Mr President"/>
-            </datalist>
-          </div>
-          <div className="client-info-col">
-            <label htmlFor="frmEmailA">Email</label>
-            <input type="email" name="email" id="frmEmailA" autoComplete="email" required placeholder="name@example.com"/>
-          </div>
+class Contact extends React.Component { 
+
+  onSubmit(event: React.FormEvent<HTMLInputElement>) {
+    event.preventDefault();
+    
+  }
+  
+  render() {
+    
+    return (
+      <article id="contact" className="contact">
+        <div className="form-wrapper">
+          <div className="reach-out">
+            <h2>HOW CAN I HELP?</h2>
+            <p>Thanks for visiting!<br/>Do you have a project in need of a developer? Want to collaborate? Let's work together!<br/>Give me an idea of what you're looking for below.</p>
+          </div>      
+          <form>
+            <fieldset>
+              <legend>Contact me</legend>
+              <div className="form-split">
+                <div className="info-col">
+                  <div className="input-spacer">
+                    <label htmlFor="frmNameA">Name (required)</label>
+                    <input required type="text" name="name" id="frmNameA" autoComplete="name" placeholder="Bazos" list="names"/>
+                    <datalist>
+                      <option value="Darth Vader"/>
+                      <option value="Mr President"/>
+                    </datalist>
+                  </div>
+                </div>
+                <div className="info-col">
+                  <div className="input-spacer">
+                    <label htmlFor="frmEmailA">Email (required)</label>
+                    <input required type="email" name="email" id="frmEmailA" autoComplete="email" placeholder="name@example.com"/>
+                  </div>
+                </div>
+              </div>
+              <div className="input-spacer">
+                <label>Tell me about your project (required)</label>
+                <textarea required id="about-project" placeholder="The next big thing"/>
+              </div>
+              <div className="input-spacer">
+                <label htmlFor="content">Anything else?</label>
+                <textarea id="content" placeholder="..."/>
+              </div>
+              <input onClick={(event) => this.onSubmit(event)} type="submit" name="submit" id="submit" className="submit-btn"/>
+            </fieldset>
+          </form>
         </div>
-        <label htmlFor="content">Message</label>
-        <textarea id="content" placeholder="You're hired"/>
-        <input type="submit" name="submit" id="submit" className="submit-btn"/>
-      </form>
-    </div>
-  </article>
-);
+      </article>
+    )
+  };
+};
 
 export default Contact;
